786de2154cf9a144f7a9e049848d4fd3ca607859
Bryan Hirsch <bryhirschPKG@gmail.com>
2020-06-22 18:34:44 -0700

    Added settings configuration and made game replayable, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-#This is a simple number guessing game in python.
-#As of now it only does numbers 1-20, but in the future I would like to add configuration parameters for different ranges.
-#Currently I am adding exception handling for user input.
+#This is a simple number guessing game in python. With configurable settings.
+I have made it give options(play, change settings, or quit), repeating until a valid option is given.
+I have added the ability to change the range and max guesses allowed
+I have exception handling to ensure expected int inputs to be valid integers. Only other input would be user name, which can be any valid input.
+
+The only other thing I would add to this would be further checking of out of range guesses, and penalties for repeated invalid guesses.
--- guess.py
+++ guess.py
@@ -1,25 +1,28 @@
-# This is a guess the number game
-# Plans: allow choosing the range, make out of range exception, make game a function and infinite loop "would you like to play again or quit(sys.exit())", have game bully you if you input several invalid guesses(error counter, maybe even punish you/increment guessesTaken)
+# This is a guess the number game - You can change the range and max guesses allowed
+# Plans: Test this from shell execution
 
 import random
+import sys
 
+def intInput():
+    while True:
+        try:
+            num = int(input())
+            return num
+        except ValueError:
+            print('Error: You did not enter a valid number.')
+            continue
 
-if __name__ == "__main__":
-    print('Hello. What is your name?')
-    name = input()
-
-    print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
-    secretNumber = random.randint(1, 20)
+def play(settings):
+    print('I am thinking of a number between ' + str(settings[0]) + ' and ' + str(settings[1]) + '.')
+    secretNumber = random.randint(settings[0], settings[1])
 
     guessesTaken = 0
     while True:
         print('Take a guess.')
-        try:
-            guess = int(input())
-            if guess < 1 or guess > 20:
-                raise ValueError    # I plan to make this a custom exception "Out of Range"
-        except ValueError:
-            print('Error: You did not enter a valid number.')
+        guess = intInput()
+        if guess < settings[0]  or guess > settings[1]: # If the guess is not in range, it doesn't count and the user must guess again
+            print('That number is not in between ' + str(settings[0]) + ' and ' + str(settings[1]) + ', try again.')
             continue
         guessesTaken += 1
         if guess < secretNumber:
@@ -28,10 +31,46 @@ if __name__ == "__main__":
             print('Your guess is too high.')
         else:
             break # This condition is for the correct guess!
-        if guessesTaken > 6:
+        if guessesTaken >= settings[2]:
             break # This condition is if the guess limit has been reached
 
     if guess == secretNumber:
         print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses!')
     else:
         print('Nope. The number I was thinking of was ' + str(secretNumber) + '.')
+    return None
+
+def setSettings():
+    while True:
+        print('Please enter a low value.')
+        lowest = intInput()
+        print('Please enter a high value.')
+        highest = intInput()
+        print('Please enter the max number of guess you would like.')
+        maxGuesses = intInput()
+        if lowest > highest or maxGuesses < 1:
+            print("Something isn't right, your lowest(" + str(lowest) + ") cannot be greater than your highest(" + str(highest) + ") and your max guesses(" + str(maxGuesses) + ") must be at least 1.")
+        else:
+            return (lowest, highest, maxGuesses)
+
+if __name__ == "__main__":
+    print('Hello. What is your name?')
+    name = input()
+
+    #Settings variables, hardcoded for default, tuple (lowest, highest, maxGuesses)
+    settings = (1, 20, 6)
+
+    while True:
+        print('Well, ' + name + ', what would you like to do? Enter a number: (1) Play, (2) Change settings, or (0) Quit')
+        action = intInput()
+        if action == 0:
+            print('Goodbye!')
+            sys.exit(0)
+        elif action == 1:
+            play(settings)
+            print("That was fun, let's play again!")
+        elif action == 2:
+            settings = setSettings()
+            print('Settings have been updated: Low value=' + str(settings[0]) + ', High value=' + str(settings[1]) + ', Max guesses=' + str(settings[2]))
+        else:
+            print('Action not found')
